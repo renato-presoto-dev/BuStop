0916cec7bbfff8496ea2af67f5ad1de548315e4b
Renato Presoto <renatopresoto.dev@gmail.com>
2026-02-02 17:46:48 -0300

    007: Pagina Horarios estrutura básica

--- bustop-app/src/app/pages/horarios-page/horarios-page.component.ts
+++ bustop-app/src/app/pages/horarios-page/horarios-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-horarios-page',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './horarios-page.component.html',
   styleUrl: './horarios-page.component.css'
 })
