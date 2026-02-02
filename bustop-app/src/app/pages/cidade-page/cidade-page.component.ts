19b41ad35aa10a1e883fee57882264dd68fa6e13
Renato Presoto <renatopresoto.dev@gmail.com>
2026-02-02 17:06:53 -0300

    004: Pagina cidade estrutura básica

--- bustop-app/src/app/pages/cidade-page/cidade-page.component.ts
+++ bustop-app/src/app/pages/cidade-page/cidade-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-cidade-page',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule, MatInputModule, MatSelectModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './cidade-page.component.html',
   styleUrl: './cidade-page.component.css'
 })
