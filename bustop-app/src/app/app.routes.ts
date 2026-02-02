46c62ea8a590d4576bb6f94742c8c290d1fb15b4
Renato Presoto <renatopresoto.dev@gmail.com>
2026-02-02 16:20:37 -0300

    002: Rotas de navegação entre paginas

--- bustop-app/src/app/app.routes.ts
+++ bustop-app/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ApoioPageComponent } from './pages/apoio-page/apoio-page.component';
+import { CidadePageComponent } from './pages/cidade-page/cidade-page.component';
+import { HorariosPageComponent } from './pages/horarios-page/horarios-page.component';
+import { ConfigPageComponent } from './pages/config-page/config-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginPageComponent,
+        title: 'Bustop - Bem vindo'
+      },
+      {
+        path: 'apoio',
+        component: ApoioPageComponent,
+        title: 'Apoie-nos'
+      },
+      {
+        path: 'cidade',
+        component: CidadePageComponent,
+        title: 'Escolha sua cidade'
+      },
+      {
+        path: 'horarios',
+        component: HorariosPageComponent,
+        title: 'Horarios do terminal'
+      },
+      {
+        path: 'config',
+        component: ConfigPageComponent,
+        title: 'Configurações'
+      },
+      {
+        path: 'main',
+        component: MainPageComponent,
+        title: 'Bustop'
+      },
+      { path: '', redirectTo: 'main', pathMatch: 'full' }
+];
